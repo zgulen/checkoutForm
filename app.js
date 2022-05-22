51336c18921c5bc521d499e21d7ea71d49923217
zgulen <gulenzubeyir@gmail.com>
2022-05-22 17:52:28 +0300

    Finished

---
+++ app.js
@@ -0,0 +1,10 @@
+const userInput = document.getElementById("valid");
+const output = document.getElementById("demo");
+const regex = 
+
+
+console.log(userInput);
+
+userInput.addEventListener("keyup", () =>{
+    console.log(userInput.value);
+})
